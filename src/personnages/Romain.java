794c14ebbe4a0059fbbab1339fbf985dde8c606c
AlexxRhodes <alex.rhodes@univ-tlse3.fr>
2024-01-19 09:33:18 +0100

    Questions 4.7

---
+++ src/personnages/Romain.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Romain {
+	String name;
+	int strength;
+	
+	public Romain(String n, int s)
+	{
+		name = n;
+		strength = s;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+}
