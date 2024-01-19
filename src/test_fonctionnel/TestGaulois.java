e7ca916afb59be0443068bdd98918f7da37625e3
AlexxRhodes <alex.rhodes@univ-tlse3.fr>
2024-01-19 09:10:40 +0100

    Questions 4.3

---
+++ src/test_fonctionnel/TestGaulois.java
@@ -0,0 +1,9 @@
+package test_fonctionnel;
+
+import personnages.Gaulois;
+
+public class TestGaulois {
+	public static void main(String[] args) {
+		Gaulois Asterix = new Gaulois("Asterix", 8);
+	}
+}
